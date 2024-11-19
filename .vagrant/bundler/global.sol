21ff0894d746dfd1b02aad3c0b4ba6b32d654711
Benedict Williams <benedict.williams@gmail.com>
2024-11-19 15:28:03 -0800

    tidying up

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"fc8abcf1a482ec83912988dc460658e0ce13bfda0291da0879c932a1f7bc1798","vagrant_version":"2.4.3"}
